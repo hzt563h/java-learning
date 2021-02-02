3d754ebc7c5c8bcf8109975eae82a295aca0b6e8
黄泽涛 <huangzetao@zhuojianchina.com>
2021-02-02 20:33:24 +0800

    实现 Runnable 接口demo

---
+++ src/main/java/com/hzt/demo/Main.java
@@ -0,0 +1,11 @@
+package com.hzt.demo;
+
+/**
+ * @author huangzetao
+ * @version 1.0.0
+ * @ClassName Main.java
+ * @Description TODO 主启动类
+ * @createTime 2021年02月02日 20:29:00
+ */
+public class Main {
+}
---
+++ src/main/java/com/hzt/demo/thread/MyThread.java
@@ -0,0 +1,20 @@
+package com.hzt.demo.thread;
+
+/**
+ * @author huangzetao
+ * @version 1.0.0
+ * @ClassName MyThread.java
+ * @Description TODO 实现 Runnable 接口
+ * @createTime 2021年02月02日 20:30:00
+ */
+public class MyThread implements Runnable {
+    public void run() {
+        System.out.println("我是另一个线程");
+    }
+
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        Thread thread = new Thread(myThread);
+        thread.start();
+    }
+}
