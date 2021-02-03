86b2e3bba8c71a68556946d1f885a76ecc57767d
黄泽涛 <huangzetao@zhuojianchina.com>
2021-02-03 16:51:15 +0800

    IntegerDemo添加说明

--- src/main/java/com/hzt/demo/base/IntegerDemo.java
+++ src/main/java/com/hzt/demo/base/IntegerDemo.java
@@ -10,6 +10,11 @@ package com.hzt.demo.base;
 public class IntegerDemo {
     /**
      * 在 Java 8 中，Integer 缓存池的大小默认为 -128~127。
+     * 在 jdk 1.8 所有的数值类缓冲池中，Integer 的缓冲池 IntegerCache 很特殊
+     * 这个缓冲池的下界是 - 128，上界默认是 127，但是这个上界是可调的，
+     * 在启动 jvm 的时候，通过 -XX:AutoBoxCacheMax=<size> 来指定这个缓冲池的大小，
+     * 该选项在 JVM 初始化的时候会设定一个名为 java.lang.IntegerCache.high 系统属性
+     * 然后 IntegerCache 初始化的时候就会读取该系统属性来决定上界。
      * @param args
      */
     public static void main(String[] args) {
