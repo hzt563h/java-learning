3df81d742a233dfe109b350608b6ee6476175b8c
黄泽涛 <huangzetao@zhuojianchina.com>
2021-02-03 14:56:28 +0800

    await() signal() signalAll()

---
+++ src/main/java/com/hzt/demo/thread/AwaitSignalExample.java
@@ -0,0 +1,47 @@
+package com.hzt.demo.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author huangzetao
+ * @version 1.0.0
+ * @ClassName AwaitSignalExample.java
+ * @Description TODO
+ * @createTime 2021年02月03日 14:54:00
+ */
+public class AwaitSignalExample {
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+    public void before() {
+        lock.lock();
+        try {
+            System.out.println("before");
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void after() {
+        lock.lock();
+        try {
+            condition.await();
+            System.out.println("after");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        AwaitSignalExample example = new AwaitSignalExample();
+        executorService.execute(() -> example.after());
+        executorService.execute(() -> example.before());
+    }
+}
