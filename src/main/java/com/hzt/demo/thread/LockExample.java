016fcbd615306eeb6a9132e43eff5d479c2f6297
黄泽涛 <huangzetao@zhuojianchina.com>
2021-02-03 14:43:31 +0800

    ReentrantLock

---
+++ src/main/java/com/hzt/demo/thread/LockExample.java
@@ -0,0 +1,34 @@
+package com.hzt.demo.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author huangzetao
+ * @version 1.0.0
+ * @ClassName LockExample.java
+ * @Description TODO
+ * @createTime 2021年02月03日 14:42:00
+ */
+public class LockExample {
+    private Lock lock = new ReentrantLock();
+
+    public void func() {
+        lock.lock();
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.print(i + " ");
+            }
+        } finally {
+            lock.unlock(); // 确保释放锁，从而避免发生死锁。
+        }
+    }
+    public static void main(String[] args) {
+        LockExample lockExample = new LockExample();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(() -> lockExample.func());
+        executorService.execute(() -> lockExample.func());
+    }
+}
