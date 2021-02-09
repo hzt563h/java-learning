1fb02997351b0a5e4eebc3c43d41a492addd4b15
黄泽涛 <huangzetao@zhuojianchina.com>
2021-02-09 16:25:31 +0800

    双重校验锁实现对象单例（线程安全）

---
+++ src/main/java/com/hzt/demo/designpattern/singleton/Singleton.java
@@ -0,0 +1,28 @@
+package com.hzt.demo.designpattern.singleton;
+
+/**
+ * @author huangzetao
+ * @version 1.0.0
+ * @ClassName Singleton.java
+ * @Description TODO
+ * @createTime 2021年02月09日 15:45:00
+ */
+public class Singleton {
+    private volatile static Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getUniqueInstance() {
+        //先判断对象是否已经实例过，没有实例化过才进入加锁代码
+        if (uniqueInstance == null) {
+            //类对象加锁
+            synchronized (Singleton.class) {
+                if (uniqueInstance == null) {
+                    uniqueInstance = new Singleton();
+                }
+            }
+        }
+        return uniqueInstance;
+    }
+}
